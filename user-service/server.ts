3af81bbb14bfec7a55cc546e56c08777ae42696f
Dinar2202 <dinar2202@gmail.com>
2025-10-08 11:35:55 +0300

    first commit

---
+++ user-service/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+import { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);

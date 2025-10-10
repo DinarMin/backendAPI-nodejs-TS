3e48ae22870552f8d35ba85dc7fea700da2ba789
Dinar2202 <dinar2202@gmail.com>
2025-10-10 09:48:23 +0300

    refactor: creating a server in a separate folder

---
+++ api-gateway/app/app.ts
@@ -0,0 +1,51 @@
+import express from "express";
+import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import cors from "cors";
+import axios from "axios";
+import { Request, Response, NextFunction } from "express";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  handler: (req, res) => {
+    res.status(429).json({
+      success: false,
+      message: "Слишком много запросов!",
+    });
+  },
+});
+
+export const app = express();
+
+dotenv.config();
+app.use(cors());
+app.use(helmet());
+app.use("/", limiter);
+app.use(express.json());
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error("Необработанная ошибка:", err);
+  res.status(500).json({ error: err.message });
+});
+
+const USER_SERVICE_URL: string = `http://localhost:${process.env.PORTUSERSERVICE}`;
+
+app.post("/registration", async (req, res) => {
+  try {
+    const response = await axios.post(
+      `${USER_SERVICE_URL}/user/registration`,
+      req.body,
+      {
+        validateStatus: (status) => status < 500,
+      }
+    );
+    res.status(response.status).json(response.data);
+  } catch (error: any) {
+    console.error(error);
+    res.status(error.response?.status || 500).json({
+      success: false,
+      message: error.response?.data?.message || error.message,
+    });
+  }
+});
--- api-gateway/server.ts
+++ api-gateway/server.ts
@@ -1,29 +1,7 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import rateLimit from "express-rate-limit";
-import helmet from "helmet";
-import { Request, Response, NextFunction } from "express";
-import axios from "axios";
-// import { createProxyMiddleware } from "http-proxy-middleware";
+import { app } from "./app/app.js";
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  handler: (req, res) => {
-    res.status(429).json({
-      success: false,
-      message: "Слишком много запросов!",
-    });
-  },
-});
-
-const app = express();
-app.use(cors());
-app.use(helmet());
-app.use("/", limiter);
 dotenv.config();
-app.use(express.json());
 
 const PORTApigateway: number = Number(process.env.PORTAPIGATEWAY || 3000);
 
@@ -46,30 +24,4 @@ server.on("error", (err: Error) => {
     console.error("Неизвестная ошибка сервера:", err);
   }
   process.exit(1);
-});
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error("Необработанная ошибка:", err);
-  res.status(500).json({ error: err.message });
-});
-
-const USER_SERVICE_URL: string = `http://localhost:${process.env.PORTUSERSERVICE}`;
-
-app.post("/registration", async (req, res) => {
-  try {
-    const response = await axios.post(
-      `${USER_SERVICE_URL}/user/registration`,
-      req.body,
-      {
-        validateStatus: (status) => status < 500,
-      }
-    );
-    res.status(response.status).json(response.data);
-  } catch (error: any) {
-    console.error(error);
-    res.status(error.response?.status || 500).json({
-      success: false,
-      message: error.response?.data?.message || error.message,
-    });
-  }
 });

30857c794867565524b4804cc760e7ae826f6e52
Dinar2202 <dinar2202@gmail.com>
2025-10-11 18:38:54 +0300

    fix: the token is sent only if it is available

--- api-gateway/app/app.ts
+++ api-gateway/app/app.ts
@@ -59,15 +59,16 @@ app.post("/users/authorization", async (req, res) => {
         validateStatus: (status) => status < 500,
       }
     );
-
     const token = await response.data.token;
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      sameSite: "lax",
-      secure: false,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+    if (token) {
+      res.cookie("token", token, {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: false,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+    }
     if (response.status === 200) {
       return res
         .status(response.status)

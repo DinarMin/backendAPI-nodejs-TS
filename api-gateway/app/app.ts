95a58f5592d1f53603ce121adf60ee3c14d31fba
Dinar <dinar2202@gmail.com>
2025-12-21 13:46:07 +0300

    feat: http-proxy-middleware

--- api-gateway/app/app.ts
+++ api-gateway/app/app.ts
@@ -1,17 +1,17 @@
 import express from "express";
-import dotenv from "dotenv";
+import { Request, Response, NextFunction } from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
 import cors from "cors";
-import axios from "axios";
-import { Request, Response, NextFunction } from "express";
-import { routes } from "../routes/routes.js";
 import cookieParser from "cookie-parser";
+import { IncomingMessage, ServerResponse } from "http";
+import { routes } from "../routes/routes.js";
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 500,
-  handler: (req, res) => {
+  handler: (req: Request, res: Response) => {
     res.status(429).json({
       success: false,
       message: "Слишком много запросов!",
@@ -21,74 +21,58 @@ const limiter = rateLimit({
 
 export const app = express();
 
-dotenv.config();
-app.use(cors());
 app.use(helmet());
-app.use("/", limiter);
+app.use(cors());
+app.use(cookieParser());
+
+app.use(limiter);
+
+Object.entries(routes).forEach(([route, target]: [string, string]) => {
+  console.log(route, target);
+  const proxyOptions = {
+    target,
+    changeOrigin: true,
+    selfHandleResponse: true,
+    pathRewrite: (path: any, req: Request) => {
+      return req.originalUrl;
+    },
+    on: {
+      proxyRes: (proxyRes: IncomingMessage, req: Request, res: Response) => {
+        let chunks: Buffer[] = [];
+        proxyRes.on("data", (chunk: Buffer) => {
+          chunks.push(chunk);
+        });
+        proxyRes.on("end", () => {
+          const rawBody: string = Buffer.concat(chunks).toString();
+          try {
+            const parseBody = JSON.parse(rawBody);
+            if (parseBody.token) {
+              res.cookie("token", parseBody.token, {
+                httpOnly: true,
+                sameSite: "lax",
+                secure: false,
+                maxAge: 7 * 24 * 60 * 60 * 1000,
+              });
+              res.end(parseBody.message);
+            }
+            res.end(parseBody);
+          } catch (error) {
+            res.status(proxyRes.statusCode || 500).end(rawBody);
+          }
+        });
+      },
+      error: (err: Error, req: Request, res: any) => {
+        res.status(500).json({ message: "Proxy Error", error: err.message });
+      },
+    },
+  };
+
+  app.use(route, createProxyMiddleware(proxyOptions));
+});
+
 app.use(express.json());
+
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error("Необработанная ошибка:", err);
   res.status(500).json({ error: err.message });
-});
-app.use(cookieParser());
-
-app.use(async (req, res) => {
-  const tokenRequest = req.cookies?.token;
-
-  const headers = { ...req.headers };
-  if (tokenRequest) headers.authorization = tokenRequest;
-
-  const {
-    host,
-    connection,
-    cookie,
-    "content-length": _,
-    ...safeHeaders
-  } = headers;
-
-  try {
-    const baseUrl = Object.entries(routes).find(([prefix]) =>
-      req.path.startsWith(prefix)
-    )?.[1];
-
-    if (!baseUrl) {
-      return res.status(404).json({ message: "Unknown routes" });
-    }
-
-    const targetUrl = `${baseUrl}${req.originalUrl}`;
-
-    const response = await axios({
-      method: req.method,
-      url: targetUrl,
-      data: req.body,
-      headers: safeHeaders,
-      timeout: 3000,
-      validateStatus: (status) => status === 200,
-    });
-
-    if (response.data.token) {
-      const tokenResponse = response.data.token;
-
-      if (tokenResponse) {
-        res.cookie("token", tokenResponse, {
-          httpOnly: true,
-          sameSite: "lax",
-          secure: false,
-          maxAge: 7 * 24 * 60 * 60 * 1000,
-        });
-        return res.status(response.status).json(response.data.message);
-      }
-    }
-    if (![200, 201].includes(response.status)) {
-      throw new Error();
-    }
-    return res.status(response.status).json(response.data);
-  } catch (error: any) {
-    const status = error.response?.status || 500;
-    const data = error.response?.data || {
-      error: error.message || "Internal server error",
-    };
-    console.error(`${status}: ${data}, ${error}`);
-    return res.status(status).json(data);
-  }
 });
